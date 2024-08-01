53e837f0e3bf97a0bc562d3d88fdeb1df0f94c70
shajoezhu <sha.joe.zhu@gmail.com>
2024-08-01 02:40:50 +0000

    style coe

--- R/f_assertions.R
+++ R/f_assertions.R
@@ -3,8 +3,7 @@
 ##
 
 assertPackageIsInstalled <- function(packageName) {
-    if (!requireNamespace(packageName, quietly = TRUE)) {
-        stop("Package ", sQuote(packageName), " is needed for this function to work")
-    }
+  if (!requireNamespace(packageName, quietly = TRUE)) {
+    stop("Package ", sQuote(packageName), " is needed for this function to work")
+  }
 }
-
